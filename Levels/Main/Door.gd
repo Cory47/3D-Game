f31dcdcc5aecc575b435c87524b53cba4a663f6a
kellye24 <103087185+kellye24@users.noreply.github.com>
2022-10-30 03:39:33 -0700

    doors and win/lose screens
    Animated opening doors and win/lose screens for successfully escaping or failing to escape the rooms

---
+++ Levels/Main/Door.gd
@@ -0,0 +1,23 @@
+extends StaticBody
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+# placeholder signal 
+# $AnimationPlayer.play("Open")
+
+
+func _on_Player_head_reveal():
+	pass # Replace with function body.
---
+++ door.gd
@@ -0,0 +1,25 @@
+extends Spatial
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+var state = "closed"
+
+func _on_Player_head_reveal():
+	
+	if state == "open":
+		pass
+	elif state == "" or state == "closed":
+		$OpenDoor.play("door")
+		state = "closed"
